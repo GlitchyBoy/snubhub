c9c7a3e05ca5c593c758fa9cdc5f02a52ed006e8
GlitchyBoy <73643402+GlitchyBoy@users.noreply.github.com>
2023-12-08 19:51:24 +0100

    Snub version 1.

---
+++ javascriptjo.js
@@ -0,0 +1,20 @@
+function Balls () {
+  alert("yo");
+  
+}
+
+function login () {
+  let emailfill = document.getElementById("email").value;
+  let passwordfill = document.getElementById("password").value;
+
+  let password = "Felix1";
+  let email = "Felix1";
+
+  if (emailfill === email & password === passwordfill) {
+    window.open('snub.html')
+    window.close('login.html') 
+  } else {
+    alert("no!")
+  }
+
+}
